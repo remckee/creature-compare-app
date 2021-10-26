6b6e5be0452ffd0f2996087fffa89260f5f1be03
rebecca-mckeever <rebecca.mckeever@g.austincc.edu>
2021-10-26 03:35:09 -0500

    set up info modal

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@
 
 "use strict";
 
+$(document).ready(function(){
+    // create modal object
+    var infoModal = new bootstrap.Modal(document.getElementById('info-modal'), {
+        backdrop: false,
+        keyboard: true
+    });  
+
+    // click handler for info button to show info modal
+    $("#info-btn").click(function () {
+        infoModal.show();
+    });
+});
+
