a4b8b013c56a6f3899d7abb6a63c4c2baae1cd18
rebecca-mckeever <rebecca.mckeever@g.austincc.edu>
2021-10-26 04:46:02 -0500

    set up input labels to update according to mode setting

--- js/main.js
+++ js/main.js
@@ -5,7 +5,22 @@
 
 "use strict";
 
+// check mode and update the text of the input labels accordingly
+function changeInputLabels() {
+    console.log("changed");
+    var modeSelect = document.getElementById('mode-selection');
+    if (modeSelect.value == "2-creature") {
+        document.getElementById('searchbox-1-label').textContent = "creature 1:";
+        document.getElementById('searchbox-2-label').textContent = "creature 2:";
+    } else {
+        document.getElementById('searchbox-1-label').textContent = "creature:";
+        document.getElementById('searchbox-2-label').textContent = "feature:";
+    }
+}
+
 $(document).ready(function(){
+    changeInputLabels();
+
     // create modal object
     var infoModal = new bootstrap.Modal(document.getElementById('info-modal'), {
         backdrop: false,
@@ -16,5 +31,12 @@ $(document).ready(function(){
     $("#info-btn").click(function () {
         infoModal.show();
     });
+    
+    // update input labels when mode changes
+    $("#mode-selection").change(function(){
+        changeInputLabels();
+
+    });
+
 });
 
