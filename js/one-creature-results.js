cd9685fda201789daed2179f50c2fa7502def3cf
rebecca-mckeever <rebecca.mckeever@g.austincc.edu>
2021-10-26 05:34:59 -0500

    initial commit

---
+++ js/one-creature-results.js
@@ -0,0 +1,16 @@
+/***********************************************************
+ * Name: Rebecca Mckeever
+ * CS 361
+***********************************************************/
+
+"use strict";
+
+$(document).ready(function(){
+    
+    // click handler for return to previous page button
+    $("#prev-page-btn").click(function () {
+        window.history.back();
+    });
+    
+});
+
